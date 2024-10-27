e8d7166766057d3ece90d93811b34104f6bc77d3
thaliabernalepe <thaliabernal@unicauca.edu.co>
2024-10-27 15:07:58 -0500

    correo
    implementar comunicacion asincrona

---
+++ backend/microservicio_revision/src/main/java/co/edu/unicauca/isii/proyecto_api_rest_revision/consumidor/MessageConsumer.java
@@ -0,0 +1,24 @@
+package co.edu.unicauca.isii.proyecto_api_rest_revision.consumidor;
+
+import org.springframework.stereotype.Service;
+
+import co.edu.unicauca.isii.proyecto_api_rest_revision.fachadaservicios.DTO.ConferenceDTO;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+@Service
+public class MessageConsumer {
+
+    @RabbitListener(queues = "temaconferencia.revision")
+    public void receiveMessage(ConferenceDTO objConfernciaCreada) {
+        System.out.println("Datos de la conferencia");
+        System.out.println("Enviando correo electrónico");
+        System.out.println("Id: "+objConfernciaCreada.getId());
+        System.out.println("Nombre: "+objConfernciaCreada.getNombre());
+        System.out.println("lugar: "+objConfernciaCreada.getLugar());
+        System.out.println("Fecha: "+objConfernciaCreada.getFecha());
+        System.out.println("tema: "+objConfernciaCreada.getTema());
+        System.out.println("descripcion: "+objConfernciaCreada.getDescripcion());
+        System.out.println("contacto: "+objConfernciaCreada.getContacto());         
+    }
+}
